647ba019c5617842f48fa9551c90d623e975e478
Signar Kristiansen <signar@siprosoft.com>
2021-02-20 01:11:37 +0000

    Add basic list functionallity

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<div>
+    <Pantry></Pantry>
+  </div>);
 }
 
+function Pantry() {
+  const [items, setItems] = useState<string[]>([])
+  const [newItem, setNewItem] = useState('')
+  return <div>
+    <form onSubmit={(e) => {
+      e.preventDefault()
+
+      if (newItem === '') return null
+      
+      setItems([...items, newItem])
+      setNewItem('')
+    }}>
+      <input type="text" value={newItem} onChange={e => {setNewItem(e.target.value)}}></input>
+      <button type="submit">Save</button>
+    </form>
+    <ul>
+      {items.map(item => (<li>{item}</li>))}
+    </ul>
+  </div>
+}
 export default App;
